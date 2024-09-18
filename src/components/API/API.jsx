8edd24599994b4a678100eb5eca4ad5176eaa7ca
inesktari <135965289+inesktari@users.noreply.github.com>
2024-09-18 14:17:12 +0200

    Kasa - API fetch

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -1,17 +1,17 @@
-import axios from "axios";
-// "axios" est une bibliothèque utilisée pour effectuer des requêtes HTTP.
-// elle permet de communiquer avec des serveurs pour récupérer ou envoyer des données.
-
 export const fetchProperties = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/api/properties");
+    const response = await fetch("http://localhost:8080/api/properties");
 
-    return response.data;
+    if (!response.ok) {
+      return { error: true, message: `Erreur HTTP: ${response.status}` };
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(
-      "Une erreur est survenue lors de le récupération des données.",
-      error
-    );
-    throw error;
+    return {
+      error: true,
+      message: "Erreur lors de la récupération des données.",
+    };
   }
 };
